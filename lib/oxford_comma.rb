741d994de68d8b2a93c6380563605de159160613
imtripleq <flamework89@hotmail.com>
2022-01-15 15:13:32 +1100

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size < 2
+    return array[0]
+  elsif array.size == 2
+    return array.join(' and ')
+  else
+    last = array.pop
+    result = array.join(', ')
+    result << ', and ' + last
+  end
 end
